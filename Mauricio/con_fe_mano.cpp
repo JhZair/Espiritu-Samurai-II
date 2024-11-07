d206dc3e38ca27a5a3fe8b990ff3f5380ec07719
Mauropq <pdmauricio36@gmail.com>
2024-11-07 08:09:52 -0500

    main7NOVcpp

--- Mauricio/con_fe_mano.cpp
+++ Mauricio/con_fe_mano.cpp
@@ -1,19 +1,20 @@
 #include <SFML/Graphics.hpp>
 #include <vector>
+#include <iostream> 
 
 class Cuchillo {
 public:
     sf::RectangleShape forma;
-    float velocidad = 500.0f; // Velocidad del cuchillo
+    float velocidad = 500.0f;
 
     Cuchillo(float x, float y) {
-        forma.setSize(sf::Vector2f(30.0f, 5.0f)); // Tamaño del cuchillo
+        forma.setSize(sf::Vector2f(30.0f, 5.0f));
         forma.setPosition(x, y);
-        forma.setFillColor(sf::Color::White); // Color del cuchillo
+        forma.setFillColor(sf::Color::White);
     }
 
     void mover(float tiempoDelta) {
-        forma.move(velocidad * tiempoDelta, 0.0f); // Movimiento hacia la derecha
+        forma.move(velocidad * tiempoDelta, 0.0f);
     }
 
     sf::Vector2f getPosicion() {
@@ -24,21 +25,42 @@ public:
 class Luchador {
 public:
     sf::RectangleShape rectan;
+    sf::RectangleShape hitbox;
     float velocidad;
     float gravity = 1100.5f;
     float velocityY = 0;
-    float jumpStrength = -480.0f; 
+    float jumpStrength = -480.0f;
     bool isJumping = false;
-    std::vector<Cuchillo> cuchillos; // Vector para almacenar los cuchillos
+    float maxhealth = 200;
+    float health = maxhealth;
+    int lives = 2;
+
+    std::vector<Cuchillo> cuchillos;
 
     Luchador(float x, float y, sf::Color color) {
         rectan.setSize(sf::Vector2f(50.0f, 100.0f));
         rectan.setPosition(x, y);
         rectan.setFillColor(color);
+
+        hitbox.setSize(sf::Vector2f(150.0f, 150.0f));
+        hitbox.setOrigin(50.0f, 75.0f);
+        hitbox.setPosition(rectan.getPosition());
+        hitbox.setFillColor(sf::Color(0, 0, 0, 0));
+
         velocidad = 200.0f;
     }
 
-    void move(float tiempoDelta, sf::Keyboard::Key izquierda, sf::Keyboard::Key derecha, sf::Keyboard::Key up, float pisoY) {
+  sf::Clock clock;
+    virtual void lanzarCuchillo() {
+    // Crea un nuevo cuchillo en la posición actual del jugador
+    if (clock.getElapsedTime().asSeconds() >= 2) {
+        Cuchillo cuchillo(rectan.getPosition().x + rectan.getSize().x, rectan.getPosition().y + rectan.getSize().y / 2);
+        cuchillos.push_back(cuchillo);
+        clock.restart(); // Reinicia el reloj para medir el tiempo nuevamente
+    }
+}
+
+    virtual void move(float tiempoDelta, sf::Keyboard::Key izquierda, sf::Keyboard::Key derecha, sf::Keyboard::Key up, float pisoY) {
         if (sf::Keyboard::isKeyPressed(izquierda) && rectan.getPosition().x > 0) {
             rectan.move(-velocidad * tiempoDelta, 0.0f);
         }
@@ -55,6 +77,7 @@ public:
         }
 
         rectan.move(0.0f, velocityY * tiempoDelta);
+        hitbox.setPosition(rectan.getPosition());
 
         if (rectan.getPosition().y + rectan.getSize().y >= pisoY) {
             rectan.setPosition(rectan.getPosition().x, pisoY - rectan.getSize().y);
@@ -63,29 +86,106 @@ public:
         }
     }
 
-    void lanzarCuchillo() {
-        // Crea un nuevo cuchillo en la posición actual del jugador
-        Cuchillo cuchillo(rectan.getPosition().x + rectan.getSize().x, rectan.getPosition().y + rectan.getSize().y / 2);
-        cuchillos.push_back(cuchillo);
+    void recibirAtaque(float damage) {
+        health -= damage;
+        if (health <= 0) {
+            health = maxhealth;
+            lives--;
+        }
     }
 
     void actualizarCuchillos(float tiempoDelta) {
-        // Mover cada cuchillo y eliminar los que salen de la ventana
         for (auto& cuchillo : cuchillos) {
             cuchillo.mover(tiempoDelta);
         }
         
-        // Eliminar cuchillos que salgan de la pantalla
         cuchillos.erase(
             std::remove_if(cuchillos.begin(), cuchillos.end(), [](Cuchillo& cuchillo) { return cuchillo.getPosicion().x > 800; }),
             cuchillos.end()
         );
     }
 
-    sf::Vector2f getPosicion() {
-        return rectan.getPosition();
+    void drawHealthBar(sf::RenderWindow& window, sf::Vector2f position) {
+        int anchoBarraMax = 200;
+        int altoBarra = 30;
+        float anchoBarraActual = (static_cast<float>(health) / maxhealth) * anchoBarraMax;
+
+        sf::RectangleShape barraTotal(sf::Vector2f(anchoBarraMax, altoBarra));
+        barraTotal.setPosition(position);
+        barraTotal.setFillColor(sf::Color(100, 100, 100));
+        barraTotal.setOutlineColor(sf::Color::Black);
+        barraTotal.setOutlineThickness(2);
+
+        sf::RectangleShape barraSalud(sf::Vector2f(anchoBarraActual, altoBarra));
+        barraSalud.setPosition(position);
+        barraSalud.setFillColor(sf::Color::Green);
+
+        window.draw(barraTotal);
+        window.draw(barraSalud);
     }
 };
+
+
+
+class Hanzo : public Luchador {
+public:
+    Hanzo(float x, float y, sf::Color color) : Luchador(x, y, color) {}
+    sf::Clock clock;
+    void lanzarCuchillo() override {
+        if (clock.getElapsedTime().asSeconds() >= 2) {
+        Cuchillo cuchillo1(rectan.getPosition().x + rectan.getSize().x, rectan.getPosition().y + rectan.getSize().y / 3);
+        Cuchillo cuchillo2(rectan.getPosition().x + rectan.getSize().x, rectan.getPosition().y + 2 * rectan.getSize().y / 3);
+        cuchillos.push_back(cuchillo1);
+        cuchillos.push_back(cuchillo2);
+        clock.restart();
+        }
+
+    }
+};
+
+
+
+
+class Samurai : public Luchador {
+private:
+    int remainingJumps = 45;
+
+public:
+    Samurai(float x, float y, sf::Color color) : Luchador(x, y, color) {}
+
+    void move(float tiempoDelta, sf::Keyboard::Key izquierda, sf::Keyboard::Key derecha, sf::Keyboard::Key up, float pisoY) override {
+        if (sf::Keyboard::isKeyPressed(izquierda) && rectan.getPosition().x > 0) {
+            rectan.move(-velocidad * tiempoDelta, 0.0f);
+        }
+        if (sf::Keyboard::isKeyPressed(derecha) && rectan.getPosition().x < 750) {
+            rectan.move(velocidad * tiempoDelta, 0.0f);
+        }
+
+        if (isJumping) {
+            velocityY += gravity * tiempoDelta;
+        }
+
+        if (sf::Keyboard::isKeyPressed(up) && remainingJumps > 0) {
+                isJumping = true;
+                velocityY = jumpStrength;
+                remainingJumps--;
+
+        }
+
+        rectan.move(0.0f, velocityY * tiempoDelta);
+        hitbox.setPosition(rectan.getPosition());  // Actualizar la posición de la hitbox
+
+        // Comprobar colisión con el piso
+        if (rectan.getPosition().y + rectan.getSize().y >= pisoY) {
+            rectan.setPosition(rectan.getPosition().x, pisoY - rectan.getSize().y);
+            velocityY = 0;
+            isJumping = false;
+            remainingJumps = 80;
+        }
+    }
+};
+
+
 
 class Piso {
 public:
@@ -94,66 +194,116 @@ public:
     Piso(float x, float y) {
         rectan.setSize(sf::Vector2f(800.0f, 50.0f));
         rectan.setPosition(x, y);
-        rectan.setFillColor(sf::Color::Green);
+        rectan.setFillColor(sf::Color::Black);
     }
 };
 
-int main() {
-    sf::RenderWindow window(sf::VideoMode(800, 600), "Peleitas");
 
-    Luchador jugador1(375.0f, 450.0f, sf::Color::Red);
-    Luchador jugador2(175.0f, 450.0f, sf::Color::Blue); // Segundo jugador
-    Piso piso(0.0f, 550.0f);
 
+
+class Juego {
+private:
+    sf::RenderWindow window;
+    Luchador* jugador1;
+    Luchador* jugador2;
+    Piso piso;
     sf::Clock reloj;
-    float tiempoDelta = 0.0f;
+    float tiempoDelta;
 
-    while (window.isOpen()) {
-        tiempoDelta = reloj.restart().asSeconds();
+public:
+    Juego()
+        : window(sf::VideoMode(800, 600), "Peleitas"),
+          piso(0.0f, 550.0f),
+          tiempoDelta(0.0f) {
+          jugador1 = new Hanzo(175.0f, 450.0f, sf::Color::Yellow);
+          jugador2 = new Samurai(375.0f, 450.0f, sf::Color::Red);
+    }
 
+    ~Juego() {
+        delete jugador1;
+        delete jugador2;
+    }
+
+    void ejecutar() {
+        while (window.isOpen()) {
+            procesarEventos();
+            actualizar();
+            renderizar();
+        }
+    }
+
+private:
+    void procesarEventos() {
         sf::Event event;
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
                 window.close();
             }
-            // Detectar cuando se presiona "O" para lanzar un cuchillo con jugador1
+
             if (event.type == sf::Event::KeyPressed) {
-                if (event.key.code == sf::Keyboard::O) {
-                    jugador1.lanzarCuchillo();
+                if (event.key.code == sf::Keyboard::R && jugador1->hitbox.getGlobalBounds().intersects(jugador2->rectan.getGlobalBounds())) {
+                    jugador2->recibirAtaque(20.0f);
+                    if (jugador2->lives == 0) {
+                        std::cout << "Jugador 1 gana!" << std::endl;
+                        window.close();
+                    }
                 }
-                // Detectar cuando se presiona "P" para lanzar un cuchillo con jugador2
-                if (event.key.code == sf::Keyboard::P) {
-                    jugador2.lanzarCuchillo();
+                if (event.key.code == sf::Keyboard::P && jugador2->hitbox.getGlobalBounds().intersects(jugador1->rectan.getGlobalBounds())) {
+                    jugador1->recibirAtaque(20.0f);
+                    if (jugador1->lives == 0) {
+                        std::cout << "Jugador 2 gana!" << std::endl;
+                        window.close();
+                    }
+                }
+                if (event.key.code == sf::Keyboard::Q) {
+                    jugador1->lanzarCuchillo();
+                }
+                if (event.key.code == sf::Keyboard::O) {
+                    jugador2->lanzarCuchillo();
                 }
             }
         }
+    }
 
-        // Mover y actualizar cuchillos de ambos jugadores
-        jugador1.move(tiempoDelta, sf::Keyboard::Left, sf::Keyboard::Right, sf::Keyboard::Up, piso.rectan.getPosition().y);
-        jugador2.move(tiempoDelta, sf::Keyboard::A, sf::Keyboard::D, sf::Keyboard::W, piso.rectan.getPosition().y);
+    void actualizar() {
+        tiempoDelta = reloj.restart().asSeconds();
+        jugador1->move(tiempoDelta, sf::Keyboard::A, sf::Keyboard::D, sf::Keyboard::W, piso.rectan.getPosition().y);
+        jugador2->move(tiempoDelta, sf::Keyboard::Left, sf::Keyboard::Right, sf::Keyboard::Up, piso.rectan.getPosition().y);
 
-        jugador1.actualizarCuchillos(tiempoDelta);
-        jugador2.actualizarCuchillos(tiempoDelta);
+        jugador1->actualizarCuchillos(tiempoDelta);
+        jugador2->actualizarCuchillos(tiempoDelta);
+    }
 
-        window.clear(sf::Color::Black); // Fondo negro
-
+    void renderizar() {
+        window.clear(sf::Color::Blue);
         window.draw(piso.rectan);
-        window.draw(jugador1.rectan);
-        window.draw(jugador2.rectan);
+        window.draw(jugador1->rectan);
+        window.draw(jugador2->rectan);
 
-        // Dibujar todos los cuchillos de jugador1
-        for (auto& cuchillo : jugador1.cuchillos) {
+        window.draw(jugador1->hitbox);
+        window.draw(jugador2->hitbox);
+
+        jugador1->drawHealthBar(window, sf::Vector2f(25.0f, 50.0f));
+        jugador2->drawHealthBar(window, sf::Vector2f(575.0f, 50.0f));
+
+        for (auto& cuchillo : jugador1->cuchillos) {
             window.draw(cuchillo.forma);
         }
-        // Dibujar todos los cuchillos de jugador2
-        for (auto& cuchillo : jugador2.cuchillos) {
+        for (auto& cuchillo : jugador2->cuchillos) {
             window.draw(cuchillo.forma);
         }
 
         window.display();
     }
+};
+
+
+
+
+
+int main() {
+    Juego juego;
+    juego.ejecutar();
 
     return 0;
 }
-//jugador 1 dispara con o 
-//jugadpr 2 dispara con p
